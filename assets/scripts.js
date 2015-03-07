68edbd0f3b4e333331fd897db21c5b4f02128c2d
Colin Miller <cjmiller@uw.edu>
2015-03-07 01:10:06 -0800

    Implemented basic views, beginning on functionality

---
+++ assets/scripts.js
@@ -0,0 +1,9 @@
+/*
+ * Created by Colin Miller for the 18-hour Facebook Hackathon (March 6-7, 2015)
+ */
+
+'use strict';
+
+(function($) {
+  console.log("Loaded!");
+}(window.jQuery));
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Trip Up</title>
+    <!-- <script src='https://cdn.firebase.com/js/client/2.2.1/firebase.js'></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script> -->
+    <link href="assets/style.css" rel="stylesheet" />
+    <script src="assets/scripts.js"></script>
+  </head>
+  <body>
+    <div id="main">
+      <h1>Trip Up</h1>
+      <div id="play-area">
+        <div id="runner">
+          <div class="head"></div>
+          <div class="upper-arm left">
+            <div class="lower"></div>
+          </div>
+          <div class="upper-arm right">
+            <div class="lower"></div>
+          </div>
+          <div class="leg left">
+            <div class="leg lower"></div>
+          </div>
+          <div class="leg right">
+            <div class="leg lower"></div>
+          </div>
+        </div>
+        <img class="background back" src="assets/land-0.svg" alt="Slow-moving background" />
+        <img class="background back second" src="assets/land-0.svg" alt="Slow-moving background" />
+        <img class="background back third" src="assets/land-0.svg" alt="Slow-moving background" />
+        <img class="background middle" src="assets/land-1.svg" alt="Medium-speed background" />
+        <img class="background middle second" src="assets/land-1.svg" alt="Medium-speed background" />
+        <img class="background middle third" src="assets/land-1.svg" alt="Medium-speed background" />
+        <img class="background front" src="assets/land-2.svg" alt="Medium-speed background" />
+        <img class="background front second" src="assets/land-2.svg" alt="Medium-speed background" />
+        <img class="background front third" src="assets/land-2.svg" alt="Medium-speed background" />
+      </div>
+    </div>
+  </body>
+</html>
